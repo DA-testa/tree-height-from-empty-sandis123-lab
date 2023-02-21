02ecbee2a7b3a56a925960f961c7611a6e14e4b0
sandis221REB063 <sandis.sarkovskis@edu.rtu.lv>
2023-02-22 00:19:38 +0200

    Update tree_height.py

--- tree_height.py
+++ tree_height.py
@@ -31,29 +31,23 @@ def compute_height(n, parents):
     return get_height(root)
 
 def get_input():
-    source = input("Enter input type (I for keyboard input, F for file input): ")
-    while source.upper() not in ['I', 'F']:
-        source = input("Enter a valid input type (I for keyboard input, F for file input): ")
-    if source.upper() == 'I':
-        n = int(input("Enter the number of nodes: "))
-        parents = list(map(int, input("Enter the parents of each node (space-separated): ").split()))
-        return n, parents
-    elif source.upper() == 'F':
-        while True:
-            file_name = input("Enter the file name: ")
-            if "a" in file_name.lower():
-                print("File name cannot contain the letter 'a'.")
-            else:
-                break
+    source = input("Enter a valid input type (I for keyboard input, F for file input): ")
+    n = None
+    parents = None
+
+    if source == 'I':
+        n = int(input())
+        parents = list(map(int, input().split()))
+    elif source == 'F':
+        filename = input("Enter the input file name: ")
         try:
-            with open(os.path.join("folder", file_name)) as f:
-                input_lines = f.readlines()
-        except FileNotFoundError:
-            print("Error: file not found.")
-            return None, None
-        n = int(input_lines[0])
-        parents = list(map(int, input_lines[1].strip().split()))
-        return n, parents
+            with open(filename, 'r') as f:
+                n = int(f.readline())
+                parents = list(map(int, f.readline().split()))
+        except EOFError:
+            pass
+
+    return n, parents
 
 def main():
     n, parents = get_input()
